f0b0f89f9353b0128ffd2a5e0549a642f024fda8
rashmi <nayakrashmi16@gmail.com>
2018-02-08 19:47:43 -0800

    Added Search feature

--- excel/app/controllers/products_controller.rb
+++ excel/app/controllers/products_controller.rb
@@ -1,10 +1,23 @@
 class ProductsController < ApplicationController
 	def index
-  		@products = Product.order(:name)
-  		respond_to do |format|
-  		format.html
-  		format.csv { send_data @products.to_csv }
-  		format.xls
+  		@products = if params[:term] 
+  			Product.where('name LIKE ?',"%#{params[:term]}%",)
+  			
+  		else 
+  			Product.where(name: '')
+  			
+  		end
+  		
+  	
   	end
-	end
+	
+
+	def products_params
+  	params.require(:task).permit(:name, :complete, :due_date, :term)
+  	respond_to do |format|
+   	format.html
+  	format.csv { send_data @products.to_csv }
+  	format.xls
+  	end
+end
 end

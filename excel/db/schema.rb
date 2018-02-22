3eba968f65beda3220abd4e8bf6ce9c1aa8a70ba
rashmi <nayakrashmi16@gmail.com>
2018-02-22 11:21:51 -0800

    Updated Priority

--- excel/db/schema.rb
+++ excel/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216204156) do
+ActiveRecord::Schema.define(version: 20180217215426) do
 
   create_table "itemdetails", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer "itemID"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20180216204156) do
     t.string "pageSpan"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "priority"
+    t.string "path"
   end
 
   create_table "itemqueuedetails", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
